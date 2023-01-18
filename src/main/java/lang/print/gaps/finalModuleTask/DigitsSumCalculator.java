dc04b8ec3ba3059bcfdcb64d6dad3520aed1eb39
asha <asha88ptz@gmail.com>
2023-01-18 15:08:33 +0300

    Finish tasks

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int n1 = number % 10; number /= 10;
+        int n2 = number % 10; number /= 10;
+        int n3 = number % 10; number /= 10;
+        int n4 = number;
+        System.out.println(n1 + n2 + n3 + n4);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int last = number % 10;
+        number = number / 10;
+        int middle = number % 10;
+        number = number / 10;
+        int first = number % 10;
+        System.out.println(last * 100 + middle * 10 + first);
     }
 }
